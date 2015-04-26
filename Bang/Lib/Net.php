6680c7320bd0e2c49801b36d01593b243fcdc6eb
Bang <bang@cycu.org.tw>
2015-04-26 10:51:23 +0800

    Lib Net加入IPv4轉換功能

--- Bang/Lib/Net.php
+++ Bang/Lib/Net.php
@@ -6,6 +6,30 @@
 class Net {
 
     /**
+     * 將IP轉算為數字
+     * @param string $ip IP位置
+     * @return int 數字結果
+     */
+    public static function INET_ATON($ip) {
+        $ip = trim($ip);
+        if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_IPV6))
+            return 0;
+        return sprintf("%u", ip2long($ip));
+    }
+
+    /**
+     * 將數字轉算為IP
+     * @param int $num 數字
+     * @return string IP位置
+     */
+    public static function INET_NTOA($num) {
+        $num = trim($num);
+        if ($num == "0")
+            return "0.0.0.0";
+        return long2ip(-(4294967295 - ($num - 1)));
+    }
+
+    /**
      * 連結取得HTTP結果內容(POST)
      * @param string $url
      */
@@ -69,62 +93,5 @@ class Net {
         }
         return $curl;
     }
-
-    /**
-     * 
-     * @param type $ip_address IP位置
-     * @param type $username 使用者帳號
-     * @param type $password 使用者密碼
-     * @return ip_country_data IP位置資訊
-     */
-    public static function GetIpInfoFromMaxmind($ip_address, $username = "94833", $password = "APSIxx8Bn0Yf") {
-        $url = "https://geoip.maxmind.com/geoip/v2.1/country/{$ip_address}?pretty";
-        $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, $url);
-        curl_setopt($curl, CURLOPT_HEADER, false);
-        curl_setopt($curl, CURLOPT_ENCODING, "UTF-8");
-        curl_setopt($curl, CURLOPT_USERPWD, "{$username}:{$password}");
-        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 50);
-        curl_setopt($curl, CURLOPT_TIMEOUT, 50);
-        curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-
-        if (substr($url, 0, 5) == "https") {
-            curl_setopt($curl, CURLOPT_PORT, 443);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-        }
-        $recall = curl_exec($curl);
-        if (!$recall) {
-            return false;
-        }
-        curl_close($curl);
-
-        var_dump($recall);
-        
-        $obj_result = json_decode($recall);
-        $area_code = $obj_result->continent->code;
-        $area = $obj_result->continent->names->en;
-        $country = $obj_result->country->names->en;
-        $country_code = $obj_result->country->iso_code;
-
-        $result = new ip_country_data();
-        $result->area_code = $area_code;
-        $result->area_name = $area;
-        $result->country_code = $country_code;
-        $result->country_name = $country;
-        $result->ip = $ip_address;
-        return $result;
-    }
-
-}
-
-class ip_country_data extends MySqlTableBase {
-
-    public $ip;
-    public $area_name;
-    public $area_code;
-    public $country_name;
-    public $country_code;
 
 }

703d654d5b46c1300d627bb797a7fcb831a2f31b
Bang <bang@cycu.org.tw>
2015-05-13 08:37:43 +0800

    调整Appstore为预设不使用memcache（以static Registry物件代替）
    感谢alan建议

--- Config.php
+++ Config.php
@@ -2,6 +2,9 @@
 
 class Config {
 
+    //是否為啟用版本 啟用版本將會不檢視錯誤訊息並記錄至資料庫中
+    const IsReleaseMode = false;
+
     /**
      * @var string 網站跟目錄（相對位置）
      */
@@ -13,19 +16,21 @@ class Config {
      */
     public static $SiteName = "Bang MVC";
 
-    //Memcached 伺服器設定
-    const MemcachedServer = "localhost";
-    const MemcachedServerPort = 11211;
     //資料庫各項連線設定
     const DbName = "bang.for.test";
     const DbHost = "localhost";
     const DbPort = "3306";
     const DbUser = "root";
     const DbPassword = "123456";
-    //各項快取設定
-    const BufferInsertCount = 10;
     //系統使用的目錄分隔符號
     const DirSplitor = "\\";
-    //是否為啟用版本 啟用版本將會不檢視錯誤訊息並記錄至資料庫中
-    const IsReleaseMode = false;
+
+}
+
+class ConfigMemecache {
+
+    const Enable = false;
+    const Host = "localhost";
+    const Port = 11211;
+
 }
--- test.php
+++ test.php
@@ -1,3 +1,7 @@
 <?php
 
 require_once 'System.php';
+
+//Appstore::Set('test1', 1234*5);
+
+echo Appstore::Get('test1');

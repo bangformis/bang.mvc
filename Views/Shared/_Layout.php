e98d916cd003dcad1cbaf773be92ed375c01cf19
Bang <bang@cycu.org.tw>
2015-07-16 18:17:15 +0800

    調整初始

--- Views/Shared/_Layout.php
+++ Views/Shared/_Layout.php
@@ -7,21 +7,22 @@ $viewBag = ViewBag::Get();
         <meta charset="UTF-8">
         <title><?= $viewBag->GetTitle() ?></title>
         <meta name="description" content="<?= $viewBag->Description ?>" />
-		<?php
-		Bundle::Css('test_css', array(
+        <?php
+        Bundle::Css('test_css', array(
             'Content/bootstrap/css/bootstrap.css',
-            'Content/bootstrap/css/bootstrap-theme.css'
+            'Content/bootstrap/css/bootstrap-theme.css',
+            'Content/css/site.css'
         ));
-		Bundle::Js('test_js', array(
+        Bundle::Js('test_js', array(
             'Content/js/lib/jquery.js',
             'Content/bootstrap/js/bootstrap.js'
         ));
-		?>
+        ?>
     </head>
     <body>
         <?php
         include Path::Content("Views/Shared/_Layout/_Header.php");
-        include Path::Content("Views/$bodyView");
+        include Path::Content("Views/{$bodyView}");
         include Path::Content("Views/Shared/_Layout/_Footer.php");
         ?>
     </body>

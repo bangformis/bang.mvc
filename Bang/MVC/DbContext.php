31d5784e43dc565c9ea64e9ecb0981775f5750ff
Bang <bang@cycu.org.tw>
2015-09-08 14:23:26 +0800

    增加DbContext 资料操作几项功能＆拿掉Sass预设开启

--- Bang/MVC/DbContext.php
+++ Bang/MVC/DbContext.php
@@ -41,4 +41,38 @@ class DbContext {
         return $stem;
     }
 
+    /**
+     * 執行Insert Query
+     * @param string $sql Prepare SQL語法
+     * @param array $params 傳入參數
+     * @return string Insert后LastId
+     */
+    public static function Insert($sql, $params) {
+        $con = DbContext::GetConnection();
+        $stem = $con->prepare($sql);
+        $stem->execute($params);
+        return $con->lastInsertId();
+    }
+
+    /**
+     * 判斷資料表是否存在
+     * @param string $table_name
+     * @return boolean 判斷結果
+     */
+    public static function IsTableExist($table_name) {
+        $sql = "show tables like '{$table_name}'";
+        $query_result = DbContext::Query($sql, array());
+        $result = $query_result->fetchAll(PDO::FETCH_ASSOC);
+        $row_count = $query_result->rowCount();
+        return $row_count > 0;
+    }
+
+    public static function StartTransaction() {
+        DbContext::Query('START TRANSACTION;');
+    }
+
+    public static function Commit() {
+        DbContext::Query('COMMIT;');
+    }
+
 }

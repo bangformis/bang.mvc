84c2072f407c1cf0cf933c8a532ed03415bdc90d
bang <bang@cycu.org.tw>
2016-01-29 17:04:03 +0800

    调整TaskResult

--- Bang/Lib/TaskResult.php
+++ Bang/Lib/TaskResult.php
@@ -5,47 +5,48 @@
  * @author Bang
  */
 class TaskResult {
-    
+
     public function __construct() {
         $this->IsSuccess = FALSE;
         $this->Message = "";
     }
-    
+
     /**
      * @var bool 是否執行成功
      */
     public $IsSuccess;
-    
+
     /**
      * @var string 結果訊息
      */
     public $Message;
-    
+
     /**
      * @var mixed 結果值
      */
     public $Value;
-    
+
     /**
      * @param string $msg
      * @return \TaskResult this
      */
-    public function SetUnsuccess($msg = ''){
+    public function SetUnsuccess($msg = '', $value = null) {
         $this->Message = $msg;
         $this->IsSuccess = FALSE;
+        $this->Value = $value;
         return $this;
     }
-    
+
     /**
      * @param mixed $value
      * @param string $msg
      * @return \TaskResult this
      */
-    public function SetSuccess($value = '' , $msg = ''){
+    public function SetSuccess($value = '', $msg = '') {
         $this->IsSuccess = true;
         $this->Message = $msg;
         $this->Value = $value;
         return $this;
     }
-    
+
 }

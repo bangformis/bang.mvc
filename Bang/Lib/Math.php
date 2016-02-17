ac6328b802062d7c71119b68ea8d19fa5d178124
bang <bang@cycu.org.tw>
2016-02-17 15:14:52 +0800

    增加Math::To36Carry功能

--- Bang/Lib/Math.php
+++ Bang/Lib/Math.php
@@ -5,6 +5,20 @@
  */
 class Math {
 
+    public static function Get36CarryMap() {
+        $map = array(
+            0 => '0', 1 => '1', 2 => '2', 3 => '3', 4 => '4',
+            5 => '5', 6 => '6', 7 => '7', 8 => '8', 9 => '9',
+            10 => 'a', 11 => 'b', 12 => 'c', 13 => 'd', 14 => 'e',
+            15 => 'f', 16 => 'g', 17 => 'h', 18 => 'i', 19 => 'j',
+            20 => 'k', 21 => 'l', 22 => 'm', 23 => 'n', 24 => 'o',
+            25 => 'p', 26 => 'q', 27 => 'r', 28 => 's', 29 => 't',
+            30 => 'u', 31 => 'v', 32 => 'w', 33 => 'x', 34 => 'y',
+            35 => 'z'
+        );
+        return $map;
+    }
+
     /**
      * @param int $n C n取r的n
      * @param int $r C n取r的r
@@ -45,6 +59,22 @@ class Math {
         } else {
             return 1;
         }
+    }
+
+    public static function To36Carry($int) {
+
+        $result = "";
+        $map = Math::Get36CarryMap();
+        $floor = $int;
+        do {
+            $floor = floor($floor / 36);
+            $mod = $floor % 36;
+            $result = $map[$mod] . $result;
+        } while ($floor > 36);
+        $mod = $floor % 36;
+        $result = $map[$mod] . $result;
+
+        return $result;
     }
 
 }

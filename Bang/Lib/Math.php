ea3ad2c856735d1f19340b0f5fd51c8b3994dd26
Bang <bang@cycu.org.tw>
2016-02-18 14:34:32 +0800

    調整36進位功能

--- Bang/Lib/Math.php
+++ Bang/Lib/Math.php
@@ -19,6 +19,19 @@ class Math {
         return $map;
     }
 
+    public static function Get36CarryUnMap() {
+        $map = array('0' => 0, '1' => 1, '2' => 2, '3' => 3, '4' => 4,
+            '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9,
+            'a' => 10, 'b' => 11, 'c' => 12, 'd' => 13, 'e' => 14,
+            'f' => 15, 'g' => 16, 'h' => 17, 'i' => 18, 'j' => 19,
+            'k' => 20, 'l' => 21, 'm' => 22, 'n' => 23, 'o' => 24,
+            'p' => 25, 'q' => 26, 'r' => 27, 's' => 28, 't' => 29,
+            'u' => 30, 'v' => 31, 'w' => 32, 'x' => 33, 'y' => 34,
+            'z' => 35
+        );
+        return $map;
+    }
+
     /**
      * @param int $n C n取r的n
      * @param int $r C n取r的r
@@ -65,14 +78,17 @@ class Math {
 
         $result = "";
         $map = Math::Get36CarryMap();
-        $floor = $int;
+        $floor = doubleval($int);
         do {
+            $mod = intval(bcmod($floor, 36));
             $floor = floor($floor / 36);
-            $mod = $floor % 36;
             $result = $map[$mod] . $result;
-        } while ($floor > 36);
+        } while ($floor >= 36);
         $mod = $floor % 36;
-        $result = $map[$mod] . $result;
+        if ($mod > 0) {
+            $result = $map[$mod] . $result;
+        }
+
 
         return $result;
     }

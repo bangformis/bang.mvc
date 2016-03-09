7ac1ae6a3bd0f699ac075140c9390951b8eff0ed
Bang <bang@cycu.org.tw>
2016-03-09 19:54:48 +0800

    各项调整

--- Bang/Lib/Checker.php
+++ Bang/Lib/Checker.php
@@ -13,7 +13,7 @@ class Checker {
             return true;
         }
     }
-    
+
     /**
      * 检查是否为TUID
      * @param string $tuid
@@ -24,6 +24,17 @@ class Checker {
         $pattern = "/^({$bit_reg}{8}\-{$bit_reg}{4}\-{$bit_reg}{4}\-{$bit_reg}{4}\-{$bit_reg}{12})$/";
         $result = preg_match($pattern, $tuid);
         return $result == 1;
+    }
+
+    /**
+     * @param string $value
+     * @return boolean
+     */
+    public static function IsPostiveInt($value) {
+        $is_int = is_numeric($value) && !String::Contains($value, '.');
+        $is_not_null = String::IsNotNullOrSpace($value);
+        $great_than_0 = intval($value) > 0;
+        return $is_not_null && $is_int && $great_than_0;
     }
 
 }
--- test.php
+++ test.php
@@ -2,9 +2,6 @@
 
 require_once 'System.php';
 
-$test =15564453;
 
-$str_36 = Math::To36Carry($test);
-$str_10 = Math::To10CarryFrom36($str_36);
-$file = new TextFile($path);
-echo $str_36 . '=' . $str_10;
+echo Math::C(49, 3);
+

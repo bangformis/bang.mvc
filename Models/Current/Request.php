fc114a6041965d74e697686e2bac5b4781679aff
Bang <bang@cycu.org.tw>
2018-06-14 10:03:26 +0800

    設定 Request 可抓取新版本 Datetime

--- Models/Current/Request.php
+++ Models/Current/Request.php
@@ -2,21 +2,35 @@
 
 namespace Models\Current;
 
+use Bang\Lib\eDateTime;
+use DateTime;
+
 /**
  * @author Bang
  */
 class Request {
 
     private static $Time = null;
+    private static $eDateTime = null;
 
     /**
      * @return DateTime
      */
     public static function GetDatetime() {
-        if (is_null(Request::$Time)) {
-            Request::$Time = new \DateTime();
+        if (is_null(self::$Time)) {
+            self::$Time = new DateTime();
         }
-        return Request::$Time;
+        return self::$Time;
+    }
+
+    /**
+     * @return eDateTime
+     */
+    public static function GetLibDatetime() {
+        if (is_null(self::$eDateTime)) {
+            self::$eDateTime = new eDateTime();
+        }
+        return self::$eDateTime;
     }
 
     public static function GetYmdhisTime() {

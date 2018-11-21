3ed17d1de536d5f2a762b1722e60549a1b846ca5
Bang <bang@cycu.org.tw>
2018-11-21 14:30:34 +0800

    測試功能調整

--- Controllers/Home.php
+++ Controllers/Home.php
@@ -3,6 +3,7 @@
 namespace Controllers;
 
 use Bang\Lib\Response;
+use Bang\Lib\TaskResult;
 use Models\ControllerBase;
 
 /**
@@ -13,6 +14,12 @@ class Home extends ControllerBase\ApiControllerBase {
 
     public function Index() {
         Response::Forbidden();
+    }
+    
+    public function Success(){
+        $result = new TaskResult();
+        $result->SetSuccess();
+        return $this->Json($result);
     }
 
 }
